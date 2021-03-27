581708c798027447fe78fd6d78cfaa9f25dc8f8c
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-03-27 23:32:57 +0100

    Implement simple echo function

--- evi-api.py
+++ evi-api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/{path}")
+async def echo(path):
+    return {"reqpath": path}
+    

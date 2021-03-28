6e3b7c8f149efca61e3b86f0fa31ac07d0b52e02
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-03-28 16:23:19 +0200

    Add base url

--- evi-api.py
+++ evi-api.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
+from os import path
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"reqpath": ""}
+version: int = 1
+basepath: str = "/" + path.join("api", f"v{version}")
 
-@app.get("/api/presets")
+@app.get(basepath)
 async def presets():
-
-    return {"reqpath": path}
+    return {"success": True}
     

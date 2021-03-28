e2aa83cc6cb74145e75b50d90bfc48d7aa12f33b
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-03-28 01:07:16 +0100

    Add root path

--- evi-api.py
+++ evi-api.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"reqpath": ""}
+
 @app.get("/{path}")
 async def echo(path):
     return {"reqpath": path}

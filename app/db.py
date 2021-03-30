53b8efd58edca0966b3eb71614e9e665bd74a544
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-03-31 00:39:35 +0200

    Implement database as class

--- app/db.py
+++ app/db.py
@@ -1,17 +1,47 @@
 import sqlalchemy
 import os
+import random
 from sqlalchemy import *
+from app.models import Preset
 from os import path
 
 # https://docs.sqlalchemy.org/en/14/core/tutorial.html
 
-def initConn(dbname):
-    engine = sqlalchemy.create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
-    with engine.begin() as conn:
-        table = Table()
+class DB:
+    def __init__(self, path=""):
+        engine = sqlalchemy.create_engine(f"sqlite+pysqlite:///{path}", echo=True, future=True)
+        with engine.begin() as conn:
+            table = Table()
 
-        # result = conn.execute(text(""))
-        # print(result.all())
+            # result = conn.execute(text(""))
+            # print(result.all())
+
+    def generateID():
+        while True:
+            randID = random.randint(0, 2**16)
+            if not checkIfIDExists(randID):
+                break
+
+    def checkIfIDExists(randID: int):
+        pass
+
+    def getPresets():
+        pass
+
+    def getPreset(presetID: int):
+        pass
+
+    def createPreset():
+        pass
+
+    def updatePreset(presetID: int):
+        pass
+
+    def deletePreset(presetID: int):
+        pass
+
 
 if __name__ == "__main__":
-    initConn("")
+    # initConn("")
+    generateID()
+    preset1 = Preset()

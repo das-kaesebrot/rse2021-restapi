218474cc0c3c0aa6dd90513779180a529430120e
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-03-30 20:17:21 +0200

    Add auth to get all request

--- app/main.py
+++ app/main.py
@@ -10,14 +10,16 @@ version: int = 1
 basepath: str = path.join("/", "api", f"v{version}", "evi")
 presetspath: str = "presets"
 
-# Get all presets on server
 @app.get(path.join(basepath, presetspath), summary="getPresets()")
-async def getPresets(response: Response):
+async def getPresets(response: Response, x_auth_token: str = Header(None)):
     """
-    Get all presets present on the server
-    \f
-    :param item: User input.
+    Get all presets on the server
     """
+    if not checkToken(x_auth_token):
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return {
+            "error": errorMsg.Info_FalseToken
+        }
     return {}
 
 # Create a preset
@@ -30,7 +32,7 @@ async def createPreset(response: Response, x_auth_token: str = Header(None)):
         }
 
 # Get properties of a specific preset
-@app.get(path.join(basepath, presetspath, "{presetID}"), summary="getPreset(presetID)")
+@app.get(path.join(basepath, presetspath, "{presetID}"), summary="getPreset(int presetID)")
 async def getPreset(presetID: uuid.UUID, response: Response, x_auth_token: str = Header(None)):
     if not checkToken(x_auth_token):
         response.status_code = status.HTTP_401_UNAUTHORIZED
@@ -41,7 +43,7 @@ async def getPreset(presetID: uuid.UUID, response: Response, x_auth_token: str =
     return {"presetID": presetID}
 
 # Update a selected preset
-@app.put(path.join(basepath, presetspath, "{presetID}"), summary="updatePreset(presetID)")
+@app.put(path.join(basepath, presetspath, "{presetID}"), summary="updatePreset(int presetID)")
 async def updatePreset(presetID: uuid.UUID, response: Response, x_auth_token: str = Header(None)):
     if not checkToken(x_auth_token):
         response.status_code = status.HTTP_401_UNAUTHORIZED
@@ -50,7 +52,7 @@ async def updatePreset(presetID: uuid.UUID, response: Response, x_auth_token: st
         }
 
 # Delete a selected preset
-@app.delete(path.join(basepath, presetspath, "{presetID}"), summary="deletePreset(presetID)")
+@app.delete(path.join(basepath, presetspath, "{presetID}"), summary="deletePreset(int presetID)")
 async def deletePreset(presetID: uuid.UUID, response: Response, x_auth_token: str = Header(None)):
     if not checkToken(x_auth_token):
         response.status_code = status.HTTP_401_UNAUTHORIZED

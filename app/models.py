74c7edc94636259cd195414c0470dc46d558a6f1
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-03-31 00:36:54 +0200

    Complete modeling presets class

--- app/models.py
+++ app/models.py
@@ -1,5 +1,18 @@
-import uuid
-
-class Preset
-    def __init__(self, ):
-        self.uuid = uuid.uuid4
+class Preset:
+    def __init__(
+        self,
+        id: int,
+        name: str,
+        comment: str = "",
+        saturation: float,
+        eotf: str,
+        colorGamut: str,
+        colorRange: str
+    ):
+        self.id = id
+        self.name = name
+        self.comment = comment
+        self.saturation = saturation
+        self.eotf = eotf
+        self.colorGamut = colorGamut
+        self.colorRange = colorRange
